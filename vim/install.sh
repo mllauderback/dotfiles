242ab9b55d60637eaa2528e1e397fb1d363e3867
Michael Lauderback <mllauderback@gmail.com>
2025-10-30 12:55:42 -0500

    add coc vim extension

--- vim/install.sh
+++ vim/install.sh
@@ -2,5 +2,12 @@
 
 echo "Updating vim dotfiles"
 mkdir -p $HOME/.vim
-cp $(pwd)/coc-settings.json $HOME/.vim/coc-settings.json
 cp $(pwd)/.vimrc $HOME/.vimrc
+
+# install coc extensions
+cocdir="$HOME/.config/coc"
+if [[ -d $cocdir ]]; then
+    echo "Installing coc extensions"
+    cp $(pwd)/coc-settings.json $HOME/.vim/coc-settings.json
+    cp $(pwd)/package.json $HOME/.config/coc/extensions/package.json
+    vim -c 'CocUpdateSync|q'
